9456081c1ea5355fa325fcf93401955b64c1e97c
abhaycoderarmy <2320403202@stu.manit.ac.in>
2025-08-10 21:14:08 +0530

    updated cors

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
+
 
 app.use(express.json());
 app.use(bodyParser.json());
