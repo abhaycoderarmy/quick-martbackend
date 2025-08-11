0c17320850c820bf33dcb6940adfa6a64a2d71a8
abhaycoderarmy <2320403202@stu.manit.ac.in>
2025-08-11 09:48:54 +0530

    updated cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://frontendblinkit-1.vercel.app', // Specify exact origin
+  origin: 'https://frontendblinkit.vercel.app/', // Specify exact origin
   credentials: true // Allow credentials
 }));
 
